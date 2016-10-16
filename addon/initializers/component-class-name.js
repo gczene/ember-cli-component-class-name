4ed24f46cdc37a38bca1203289ddc7a0b787668f
gczene <gabor@czene.co.uk>
2016-10-16 15:49:56 +0100

    simplified nested components

--- addon/initializers/component-class-name.js
+++ addon/initializers/component-class-name.js
@@ -3,22 +3,15 @@ import Ember from 'ember';
 export function initialize(/* application */) {
   Ember.Component.reopen({
     init() {
-      var config = Ember.getOwner(this).resolveRegistration('config:environment'),
-        slashReplacement = '-';
-
-      if (typeof config.componentClassSlashReplacement === 'string') {
-        slashReplacement = config.componentClassSlashReplacement;
-      } else if (config.componentClassSlashReplacement === false) {
-        slashReplacement = false;
-      }
 
       this._super(...arguments);
       try {
         let arr = this.toString().match(/@component:.+?:/),
-        className = arr[0].split(':')[1];
+          className = arr[0].split(':')[1];
 
-        if (slashReplacement !== false) {
-          className = className.replace(/\//g, slashReplacement);
+        // if it is nested it contains a '/'
+        if (~className.lastIndexOf('/')) {
+          className = className.substring(className.lastIndexOf('/') + 1, className.length);
         }
         this.get('classNames').pushObject(className + '-component');
       } catch (err) {
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -6,7 +6,6 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
-    componentClassSlashReplacement: true,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
